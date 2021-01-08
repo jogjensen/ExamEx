290f69ead2807090f6fa8eedacd00e30c62dd2aa
Philip Lind Jensen <jogjensen@hotmail.com>
2021-01-08 11:38:40 +0100

    Add project files.

---
+++ ExamEx/UDPWorker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace UDPBroadcaster
+{
+    class UDPWorker
+    {
+        private const int PORT = 23233;
+        private readonly Random rnd = new Random(DateTime.Now.Millisecond);
+        private const int SEC = 1;
+
+        public UDPWorker()
+        {
+
+        }
+
+        public void Start()
+        {
+
+            UdpClient client = new UdpClient();
+
+            byte[] buffer;
+
+            //Opretter måling der sender hvert sekund
+            while (true)
+            {
+                double temp = rnd.NextDouble() * 5 + 30; //laver tal mellem 30.0 og 34.999
+                double press = rnd.NextDouble() * 25 + 1000;
+                double humidity = rnd.NextDouble() * 40 + 50;
+
+
+                String str = $"temp = {temp}, press = {press}, humidity = {humidity}";
+
+                //
+                IPEndPoint SendToEP = new IPEndPoint(IPAddress.Broadcast, PORT);
+                byte[] outbuffer = Encoding.UTF8.GetBytes(str);
+                client.Send(outbuffer, outbuffer.Length, SendToEP);
+
+                Thread.Sleep(1000 * SEC);
+            }
+        }
+
+    }
+}
---
+++ UDPBroadcastRece/UDPWorker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPBroadcastRece
+{
+    class UDPWorker
+    {
+        private const int PORT = 23233;
+
+        public UDPWorker()
+        {
+
+        }
+
+        public void Start()
+        {
+            UdpClient client = new UdpClient(PORT);
+
+            byte[] buffer;
+
+            while (true)
+            {
+
+                IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, port:0);
+                buffer = client.Receive(ref remoteEP);
+
+                string str = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine("STR modtaget : \n" + str);
+
+                //todo parsing
+                string[] strs = str.Split(',');
+
+                //temp [0]
+                string tempStr = strs[0].Trim();
+                Console.WriteLine(tempStr);
+                string[] temps = tempStr.Split('=');
+                double temp = Convert.ToDouble(temps[1]);
+                Console.WriteLine("Temperature is " + temp);
+
+                //pressure [1]
+                string pressStr = strs[1].Trim();
+                Console.WriteLine(pressStr);
+                string[] press = pressStr.Split('=');
+                double pres = Convert.ToDouble(pressStr[2]);
+                Console.WriteLine("Pressure is " + pres);
+
+                //humidity [2]
+
+
+            }
+        }
+
+
+    }
+}
